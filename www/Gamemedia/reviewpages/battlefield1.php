835f16c0ad3a96b3132bce0e20ca5d95a2b44fdf
geraspru <geraspru@student.arteveldehs.be>
2017-01-06 13:04:20 +0100

    Add files via upload

---
+++ www/Gamemedia/reviewpages/battlefield1.php
@@ -0,0 +1,14 @@
+<?php
+$current = "battlefield1.php";
+require_once("../includes/headerReview.php");
+require_once("../includes/nav.php");
+?>
+<div class="container">
+    <table>
+    <div class="form-group">
+			<a href="../reviews.php"><input id="terug" name="back" type="submit" class="form-control" value="Ga terug naar reviews"></a>
+		</div>
+        </table>
+<p> geen reviews </p>
+</div>
+<?php require_once("../includes/footer.php"); ?>
